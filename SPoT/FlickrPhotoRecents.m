cee0bb3928fbc5097f748abe78fcec866c08c1fd
Matthew Evans <Matt@Evans-Family-iMac.local>
2013-03-07 22:31:43 -0500

    iPhone Portion Done

--- SPoT/FlickrPhotoRecents.m
+++ SPoT/FlickrPhotoRecents.m
@@ -14,12 +14,13 @@
 #define ALL_RESULTS_KEY @"RecentPhotos_ALL"
 #define PHOTO_KEY @"Photo"
 #define DATE_ADDED_KEY @"DateAdded"
-#define MAX_RECENT_PHOTOS 5
+#define MAX_RECENT_PHOTOS 10
 
 + (void)addPhotoToUserDefaults: (NSDictionary *) recentPhoto
 {
     NSString *recentPhotoID = recentPhoto[FLICKR_PHOTO_ID];
     NSDate *dateAdded = [NSDate date];
+    BOOL addPhoto = YES;
     
     NSMutableDictionary *mutableRecentPhotosFromUserDefaults = [[[NSUserDefaults standardUserDefaults] dictionaryForKey:ALL_RESULTS_KEY] mutableCopy];
     
@@ -27,12 +28,48 @@
         mutableRecentPhotosFromUserDefaults = [[NSMutableDictionary alloc] init];
     }
     
-    if ([mutableRecentPhotosFromUserDefaults count] >= MAX_RECENT_PHOTOS) {
-        NSLog(@"TOO MANY ENTRIES");
+    // Set Enumerator for the Keys in the Dictionary
+    NSEnumerator *enumerator = [mutableRecentPhotosFromUserDefaults keyEnumerator];
+    id key;
+    
+    // Check for a Duplicate Key to the One Being Added
+    NSString *keyToBeReplaced;
+    while ((key = [enumerator nextObject])) {
+        if ([[mutableRecentPhotosFromUserDefaults objectForKey:key][FLICKR_PHOTO_ID] isEqualToString:recentPhotoID]) {
+            addPhoto = NO;
+            keyToBeReplaced = key;
+            break;
+        }
     }
     
-    if (![mutableRecentPhotosFromUserDefaults objectForKey:recentPhotoID]) {
-        [mutableRecentPhotosFromUserDefaults setObject:@{ PHOTO_KEY : recentPhoto, DATE_ADDED_KEY : dateAdded} forKey:recentPhotoID];
+    if (addPhoto) {
+        [mutableRecentPhotosFromUserDefaults setObject:recentPhoto forKey:[dateAdded description]];
+    } else {
+        // Replace Existing Photo with New One So that It Moves to Top of List
+        [mutableRecentPhotosFromUserDefaults removeObjectForKey:keyToBeReplaced];
+        [mutableRecentPhotosFromUserDefaults setObject:recentPhoto forKey:[dateAdded description]];
+    }
+    
+    // Ensure Recents List Size is Not Too Big
+    enumerator = [mutableRecentPhotosFromUserDefaults keyEnumerator];
+    if ([mutableRecentPhotosFromUserDefaults count] > MAX_RECENT_PHOTOS) {
+        NSMutableArray *keys = [[NSMutableArray alloc] init];
+        NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
+        int keyCountToDelete = [mutableRecentPhotosFromUserDefaults count] - MAX_RECENT_PHOTOS;
+        
+        NSLog(@"Recent Photos to Delete: %d", keyCountToDelete);
+        
+        [dateFormat setDateFormat:@"yyyy-MM-dd HH:mm:ss Z"];
+        while ((key = [enumerator nextObject])) {
+            
+            [keys addObject:[dateFormat dateFromString:key]];
+        }
+        
+        NSArray *sortedKeys = [keys sortedArrayUsingSelector:@selector(compare:)];
+        for (int i = 0; i<=keyCountToDelete-1; i++) {
+            NSLog(@"Deleting: %@", [sortedKeys[i] description]);
+            [mutableRecentPhotosFromUserDefaults removeObjectForKey:[sortedKeys[i] description]];
+        }
     }
     
     [[NSUserDefaults standardUserDefaults] setObject:mutableRecentPhotosFromUserDefaults forKey:ALL_RESULTS_KEY];
@@ -49,13 +86,15 @@
 
 + (NSArray *)allRecentPhotos
 {
-    NSMutableArray *allRecentPhotos = [[NSMutableArray alloc] init];
+    NSMutableArray *sortedValues = [[NSMutableArray alloc] init];
+    NSDictionary *dict = [[NSUserDefaults standardUserDefaults] dictionaryForKey:ALL_RESULTS_KEY];
+    NSArray *sortedKeys = [[dict allKeys] sortedArrayUsingSelector: @selector(compare:)];
     
-    for (id plist in [[[NSUserDefaults standardUserDefaults] dictionaryForKey:ALL_RESULTS_KEY] allValues]) {
-        [allRecentPhotos addObject:plist[PHOTO_KEY]];
+    for (NSString *key in [sortedKeys reverseObjectEnumerator]){
+        [sortedValues addObject: [dict objectForKey: key]];
     }
     
-    return allRecentPhotos;
+    return sortedValues;
 }
 
 @end
--- SPoT/ImageViewController.m
+++ SPoT/ImageViewController.m
@@ -11,18 +11,28 @@
 @interface ImageViewController () <UIScrollViewDelegate>
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 @property (strong, nonatomic) UIImageView *imageView;
+@property (nonatomic) BOOL userDidZoom; // Tracks if the user has manually zoomed the image
 @end
 
 @implementation ImageViewController
+
+#define MINIMUM_ZOOM_SCALE 0.2
+#define MAXIMUM_ZOOM_SCALE 5.0
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	[self.scrollView addSubview:self.imageView];
-    self.scrollView.minimumZoomScale = 0.2;
-    self.scrollView.maximumZoomScale = 5.0;
+    self.scrollView.minimumZoomScale = MINIMUM_ZOOM_SCALE;
+    self.scrollView.maximumZoomScale = MAXIMUM_ZOOM_SCALE;
     self.scrollView.delegate = self;
     [self resetImage];
+}
+
+// When subviews get laid out, try to autozoom.
+- (void)viewDidLayoutSubviews
+{
+    [self autoZoom];
 }
 
 - (void)setImageURL:(NSURL *)imageURL
@@ -45,6 +55,7 @@
     if (self.scrollView) {
         self.scrollView.contentSize = CGSizeZero;
         self.imageView.image = nil;
+        self.userDidZoom = NO;
         
         NSData *imageData = [[NSData alloc] initWithContentsOfURL:self.imageURL];
         UIImage *image = [[UIImage alloc] initWithData:imageData];
@@ -61,6 +72,22 @@
 - (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView
 {
     return self.imageView;
+}
+
+- (void)scrollViewDidZoom:(UIScrollView *)scrollView
+{
+    self.userDidZoom = YES;
+}
+
+// This function will automatically set the zoom to show as much of the image as possible without showing any empty space.
+- (void)autoZoom
+{
+    if (self.imageView.image && !self.userDidZoom) { // If the imageView has an image set and the user hasn't manually zoomed yet, autozoom.
+        CGFloat widthRatio  = self.scrollView.bounds.size.width  / self.imageView.bounds.size.width;
+        CGFloat heightRatio = self.scrollView.bounds.size.height / self.imageView.bounds.size.height;
+        self.scrollView.zoomScale = (widthRatio > heightRatio) ? widthRatio : heightRatio;
+        self.userDidZoom = NO; // Setting the zoom on the previous line triggered scrollViewDidZoom, but since we know that we did the zooming we need to set the userDidZoom flag back to NO
+    }
 }
 
 
